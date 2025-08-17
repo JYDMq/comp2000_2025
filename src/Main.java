83e2fec671b0091ff582146a31161c1896318c47
JYDMq <135203642+JYDMq@users.noreply.github.com>
2025-08-18 01:22:29 +1000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import java.awt.Canvas;
 import java.awt.Dimension;
 import java.awt.Graphics;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
 
 public class Main extends JFrame {
     public static void main(String[] args) throws Exception {
@@ -19,6 +21,15 @@ public class Main extends JFrame {
       public void paint(Graphics g) {
 	g.setColor(java.awt.Color.BLACK);
 	g.drawRect(10, 10, 700, 700);
+  int h =Canvas.HEIGHT;
+  int w= Canvas.WIDTH;
+
+  int x =0;
+
+  for (int i =0; i < 20; i++){
+    x = i * (w/10);
+    g.drawLine(x, 0, x, h);
+  }
       }
     }
 
@@ -28,15 +39,7 @@ public class Main extends JFrame {
       this.setContentPane(canvas);
       this.pack();
       this.setVisible(true);
-      //int width = getWidth();
-     // int height = getHeight();
-
-      //int start=0;
-
-     // for (int i=1; i <=20; i = i++){
-     //   start= i*(width/10);
-      //  this.drawLine(start, 0, start, height);
-    //  }
+   
     }
 
     public void run() {
