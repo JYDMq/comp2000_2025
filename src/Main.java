60bef9a788dfd4785452036bd69369358ef58f5c
JYDMq <135203642+JYDMq@users.noreply.github.com>
2025-08-18 21:08:23 +1000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -12,26 +12,43 @@ public class Main extends JFrame {
       window.run();
     }
 
+        public class Cell {
+      int h =20;
+      int w= 20;
+      int l =35;
+      int gridOffset =0;
+
+    }
+
+    public class Grid {
+      Cell cell = new Cell();{
+      Paint paint= new Paint();
+      for(int i = 0; i < cell.h; i++){
+         for (int a= 0; a < cell.w; a++){
+          int x = (i * cell.l) + cell.gridOffset;
+          int y = (a * cell.l) + cell.gridOffset;
+          int o =cell.l;
+        }
+      }
+     }
+    }
+
+
+
     class Canvas extends JPanel {
       public Canvas() {
         setPreferredSize(new Dimension(720, 720));
       }
 
       @Override
+
       public void paint(Graphics g) {
-	g.setColor(java.awt.Color.BLACK);
-	g.drawRect(10, 10, 700, 700);
-  int h =Canvas.HEIGHT;
-  int w= Canvas.WIDTH;
-
-  int x =0;
-
-  for (int i =0; i < 20; i++){
-    x = i * (w/10);
-    g.drawLine(x, 0, x, h);
+    	g.setColor(java.awt.Color.BLUE);
+      g.drawRect(grid.x, grid.y, grid.o, grid.o);
+        
   }
       }
-    }
+    
 
     private Main() {
       this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
