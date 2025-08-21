f9d6bb07180243b76962fef13b968e6bc38bcb62
JYDMq <135203642+JYDMq@users.noreply.github.com>
2025-08-22 02:11:32 +1000

    color cell

--- src/Cell.java
+++ src/Cell.java
@@ -3,6 +3,7 @@ import java.awt.*;
 public class Cell {
     int x;
     int y;
+    static int size = 35;
 
 
     public Cell(int x, int y){
@@ -10,9 +11,23 @@ public class Cell {
         this.y = y;
     }
 
-    void paint (Graphics g){
-        g.setColor(java.awt.Color.BLUE);
-        g.drawRect(x, y, 35, 35);
+    void paint (Graphics g, Point mousePos){
+        if(contain(mousePos)){
+            g.setColor(Color.red);
+        } else{
+            g.setColor(Color.BLUE);
+        }
+        //g.setColor(java.awt.Color.black);
+
+        g.drawRect(x, y, size, size);
+    }
+
+    boolean contain(Point p){
+        if (p!= null){
+            return (x < p.x && x+size > p.x && y < p.y && y+size > p.y);
+        } else{
+            return false;
+        }
     }
     
 }
--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,7 @@ public class Main extends JFrame {
 
       @Override
       public void paint(Graphics g) {
-      grid.paint(g);
+      grid.paint(g, getMousePosition());
   }
       }
 
