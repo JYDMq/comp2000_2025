bd2c33df58f9ac52edc11e4b820078c71fd04e81
JYDMq <135203642+JYDMq@users.noreply.github.com>
2025-09-05 16:46:08 +1000

    task 13

--- src/StageReader.java
+++ src/StageReader.java
@@ -5,8 +5,16 @@ import java.util.List;
 
 public class StageReader {
   public static Stage readStage(String path) throws IOException {
+    try{
     Stage stage = new Stage();
     List<String> lines = Files.readAllLines(Paths.get(path));
+    
+    }
+    
+    catch(Exception a){
+      System.out.println("something is wrong");
+    }
     return new Stage();
   }
+
 }
