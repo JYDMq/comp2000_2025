6c41efa0549c45ebbbb5b3cd69c169cccc44fe55
JYDMq <135203642+JYDMq@users.noreply.github.com>
2025-09-08 23:34:45 +1000

    update

--- src/StageReader.java
+++ src/StageReader.java
@@ -5,15 +5,10 @@ import java.util.List;
 
 public class StageReader {
   public static Stage readStage(String path) throws IOException {
-    try{
+    
     Stage stage = new Stage();
     List<String> lines = Files.readAllLines(Paths.get(path));
-    
-    }
-    
-    catch(Exception a){
-      System.out.println("something is wrong unfortunate");
-    }
+    System.out.println(lines);
     return new Stage();
   }
 
