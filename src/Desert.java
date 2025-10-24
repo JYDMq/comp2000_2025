11d0fb4780add1cce1f1a1293e064101348625c2
JYDMq <135203642+JYDMq@users.noreply.github.com>
2025-10-25 01:11:37 +1100

    v0.2
    added three new files

---
+++ src/Desert.java
@@ -0,0 +1,10 @@
+import java.awt.Color;
+
+public class Desert  extends Terrain {
+
+    public Desert(Color colour, Cell l, boolean move) {
+        super(colour, l, move);
+      
+    }
+
+}
---
+++ src/Plains.java
@@ -0,0 +1,10 @@
+import java.awt.Color;
+
+public class Plains  extends Terrain {
+
+    public Plains(Color colour, Cell l, boolean move) {
+        super(colour, l, move);
+      
+    }
+
+}
