f838b54f7edf474bca4172f818f78f89f1d27767
JYDMq <135203642+JYDMq@users.noreply.github.com>
2025-10-29 22:49:49 +1100

    score

---
+++ src/ScoreTracker.java
@@ -0,0 +1,4 @@
+public class ScoreTracker {
+    int Score;
+
+}
--- src/Stage.java
+++ src/Stage.java
@@ -92,6 +92,7 @@ public class Stage {
       if(a.isBot() && a.mover != null) {
         g.drawString("mover:", labelIndent, yLoc+4*vTab);
         g.drawString(a.mover.getClass().getName(), valueIndent, yLoc+4*vTab);
+      g.drawString("Score", labelIndent, yLoc+5*vTab);
       }
     }    
   }
