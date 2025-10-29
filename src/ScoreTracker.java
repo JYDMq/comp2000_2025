82414fd7b80b1d52ce4d9a368de2d7d416f406b1
JYDMq <135203642+JYDMq@users.noreply.github.com>
2025-10-29 23:33:11 +1100

    score

--- src/ScoreTracker.java
+++ src/ScoreTracker.java
@@ -1,4 +1,24 @@
-public class ScoreTracker {
-    int Score;
+
+public  class  ScoreTracker {
+    int score =0;
+    Cell playerloc;
+    Cell itemloc;
+
+    Add add = (a, b) -> a+b;
+
+    protected ScoreTracker (Cell Ploc, Cell Iloc){
+        playerloc =  Ploc;
+        itemloc =    Iloc;
+    }
+
+    public int scoreboard(){
+
+    if(playerloc == itemloc){
+            score = add.Addition(score, 10);
+
+        }
+
+    return score;
+    }
 
 }
